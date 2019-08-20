f59bc215a03b37c94cf47f1a22447d2b66760c90
AlexisMS <alexisora@gmail.com>
2019-08-19 21:55:48 -0300

    pontuações em MidiaMomento
    cada momento pode retornar uma pontuação de acordo com a mídia selecionada

--- Assets/Scripts/Planejamento/MidiaMomento.cs
+++ Assets/Scripts/Planejamento/MidiaMomento.cs
@@ -8,16 +8,22 @@ public class MidiaMomento : MonoBehaviour, IPointerClickHandler {
     
     public ItemName initialItem;
     private ItemName item; //item no slot
-    private double points = 0;
+    //private double points = 0;
+
+    //se você tiver uma ideia melhor pra fazer isso, por favor melhore isso.
+    //está feito assim porque cada momento tem pontuações diferentes pra cada mídia, por isso todos são públicos para serem editados no unity.
+    public double livroPoints = 0;
+    public double cartazesPoints = 0;
+    public double tvPoints = 0;
 
     public void OnPointerClick(PointerEventData eventData)
     {
         //throw new System.NotImplementedException();
         //inicia seleção
 
-        Debug.Log("clicou no momento");
-        //to-do: passar própria referência para manager, inventário vai usar referência para colocar item
         GameObject.Find("PlanManager").GetComponent<PlanManager>().setSelectedMoment(gameObject);
+        Debug.Log("momento selecionado");
+        //to-do: destaque visual no momento selecionado
     }
 
     //chame essa função para mudar o item no momento
@@ -29,8 +35,10 @@ public class MidiaMomento : MonoBehaviour, IPointerClickHandler {
     public double Points() {
         //pega pontuação no contexto do momento
         switch (item) {
-            case ItemName.Livro: return points = 1;
-            default: return points = 0;
+            case ItemName.Livro: return livroPoints;
+            case ItemName.Cartazes: return cartazesPoints;
+            case ItemName.TV: return tvPoints;
+            default: return 0;
         }
     }
 
