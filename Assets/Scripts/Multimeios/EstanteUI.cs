a9290bf77574fead61e1ad1f6c262616d3dc60dc
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-08-21 05:35:26 -0300

    Coletar itens na estante da sala multimeios

---
+++ Assets/Scripts/Multimeios/EstanteUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstanteUI : MonoBehaviour {
+
+    [SerializeField]
+    private List<EstanteItemUI> itemSlots;
+
+    [SerializeField]
+    private Estante estante;
+
+    // Use this for initialization
+    void Start () {
+        DisplayItems();
+	}
+
+    public void DisplayItems()
+    {
+        // Limpar slots
+        foreach (var slot in itemSlots)
+        {
+            slot.gameObject.SetActive(false);
+        }
+
+        // Popular slots
+        var slots = itemSlots.GetEnumerator();
+        foreach (var media in estante.Items)
+        {
+            if (slots.MoveNext())
+            {
+                slots.Current.Estante = estante;
+                slots.Current.Item = media;
+            }
+        }
+    }
+}
