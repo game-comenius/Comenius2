e4f009ada2a294b4885a11fcf30a3360fb256607
HenriMRC <31775024+HenriMRC@users.noreply.github.com>
2019-09-13 18:00:51 -0300

    Limpeza Parcial do Projeto

---
+++ Assets/Scripts/DontDestroyScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyScript : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
