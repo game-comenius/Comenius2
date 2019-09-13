d3782df305092099987187e52dcc9961388e1fec
HenriMRC <31775024+HenriMRC@users.noreply.github.com>
2019-09-13 18:25:37 -0300

    StartScene inicializa o jogo corretamente

--- Assets/Scenes/StartSceneScript.cs
+++ Assets/Scenes/StartSceneScript.cs
@@ -7,8 +7,26 @@ public class StartSceneScript : MonoBehaviour
 {
     [SerializeField] private string firstSceneName;
 
+    private bool sendoUsado = false;
+
     public void ComecarJogo()
     {
+        if (!sendoUsado) 
+        {
+            StartCoroutine(InicializarManagerSceneEJogo());
+
+            sendoUsado = true;
+        }
+    }
+
+    private IEnumerator InicializarManagerSceneEJogo()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(5, LoadSceneMode.Additive);
+
+        yield return new WaitUntil(() => operation.progress == 1);
+
+        yield return null;
+
         SceneManager.LoadScene(firstSceneName);
     }
 }
