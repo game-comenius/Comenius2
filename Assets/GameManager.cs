b2a45b8c401103ac5451e39a0a3f0b128a3df3f5
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-09-13 15:43:52 -0300

    Revert "Revert "Muitas coisas""
    This reverts commit 557b39a831389ed3b9e39d5582a7123ae4a10c5d.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _gameManager;
+
+    public static GameManager gameManager
+    {
+        get
+        {
+            return _gameManager;
+        }
+    }
+
+    private static bool _uiSendoUsada = false;
+
+    public static bool uiSendoUsada
+    {
+        get
+        {
+            return _uiSendoUsada;
+        }
+
+        set
+        {
+            _uiSendoUsada = value;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_gameManager == null)
+        {
+            _gameManager = this;
+        }
+        else
+        {
+            Debug.Log("Tem 2 GameManagers");
+        }
+    }
+}
