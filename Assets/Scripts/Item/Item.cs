1f7a24fefaf55aeb5d6671d61348a2c9bc3fde15
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-09-13 15:43:37 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/game-comenius/Comenius2""
    This reverts commit bb3f1a0dd922b028fd400adf0d1e9e856b2feb2a.

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -9,6 +9,7 @@ public class Item
     public string FriendlyName { get; private set; }
     public Sprite Sprite { get; private set; }
     public string Description { get; private set; }
+    public string FullDescription { get; private set; }
 
 
     public Item(ItemName itemName)
@@ -19,19 +20,58 @@ public class Item
         {
             case ItemName.Livro:
                 FriendlyName = "Livro";
-                Description = "Descrição do Livro...";
+                Description = "Para fazer atividades, aprender e revisar o conteúdo. Um clássico!";
+                FullDescription = "É um livro de cunho pedagógico composto de conteúdo do currículo escolar. Pode ser usado para pesquisa e resolução de exercícios. A linguagem presente no livro didático é visual e principalmente textual, trazendo o conteúdo de forma intercalada entre textos e imagens.";
                 break;
             case ItemName.Cartazes:
                 FriendlyName = "Cartazes";
-                Description = "Descrição de Cartazes...";
+                Description = "Um cartaz em branco, perfeito para criar algo durante a aula";
+                FullDescription = "É uma litografia. Geralmente feitos pelos próprios estudantes, utilizados para atividades pedagógicas individuais ou em grupo para proporcionar a produção coletiva, apresentação e socialização do que foi produzido com o grupo escolar, produção de trabalhos escolares que permitem a pesquisa, produção textual, produção artística, expressar opiniões, elaboração dos conteúdos aprendidos e exposição visual de conteúdo pedagógico. Dependendo do uso pode ter um viés mais textual ou mais imagético, mas voltado para o lado visual, dado a sua possibilidade de exposição tende a ter um melhor uso para imagens trabalhadas pela turma.";
                 break;
             case ItemName.SemNome:
                 FriendlyName = "Sem nome";
                 Description = "Sem descrição";
+                FullDescription = "Sem texto";
                 break;
             case ItemName.TV:
-                FriendlyName = "TV";
-                Description = "Uma TV";
+                FriendlyName = "TV com reprodutor de VHS";
+                Description = "Sem uma fita VHS vai ser difícil achar algo de interessante passando na TV";
+                FullDescription = "É um aparelho de exibição de canais sintonizados por satélite e imagens conectados a um reprodutor VHS. Seu uso pedagógico pode ser através de programas que estejam passando ao vivo ou naquele momento, tende a motivar o aprendizado por conta do conteúdo audiovisual. Assim como o reprodutor VHS possui uma linguagem visual, podendo trabalhar em conjunto com ele, ou utilizando canais de televisão.";
+                break;
+            case ItemName.Caderno:
+                FriendlyName = "Caderno";
+                Description = "Na minha época não tinha tantas cores de canetas e adesivos divertidos…";
+                FullDescription = "É um artefato histórico atualmente produzidos em papel. Utilizadas para a anotação, expressão textual e armazenamento de informações mais relevantes para os estudantes durante as aulas, serve também de registro histórico daquela época. É um instrumento de comunicação entre o professor e o estudante, de desenvolver a coordenação motora fina, cognitiva, raciocínio lógico, de audição, de visão e também como instrumento de avaliação e de caligrafia. Possui uma linguagem muito versátil, já que pode trabalhar com a forma textual, tanto verbal, sendo ditado pelo professor, como escrita, copiando o que está no quadro, ou em alguma outra mídia. Além do uso de textos, pode-se usar de elementos visuais.";
+                break;
+            case ItemName.Gravador:
+                FriendlyName = "Gravador";
+                Description = "Para capturar os sons da natureza… Os alunos também podem usar para gravar algo durante a aula. Uma entrevista, ou ainda o que aprenderam sobre o conteúdo.";
+                FullDescription = "É um aparelho de gravação e reprodução de áudio. Pode ser utilizado possibilitando uma maior autonomia para criação de conteúdo. Possui um uso mais específico, fazendo com que sua linguagem seja praticamente apenas verbal, a sua formalidade vai depender de quem está fazendo a produção do conteúdo.";
+                break;
+            case ItemName.VHS:
+                FriendlyName = "Fita VHS";
+                Description = "Pássaros, nossos amigos penosos.";
+                FullDescription = "É uma fitas em  formato VHS (Vídeo Home System) contendo um documentário curta metragem. Seu uso pedagógico é relacionado a apresentação de conteúdos pelo meio visual, contato com outras culturas sociais e linguísticas. Utiliza de uma linguagem audiovisual, que tende a ser mais interessante para os alunos.";
+                break;
+            case ItemName.Camera:
+                FriendlyName = "Camera Fotográfica Polaroid";
+                Description = "Os alunos podem usar isso para tirar fotos durante a aula, só não sei se tem muito o que fotografar na sala de aula.";
+                FullDescription = "É um instrumento de captação de imagem estática para posterior impressão. Permite a captura de representações imagéticas do tempo histórico vivido. Seu uso pedagógico é de captar imagem e na elaboração de conteúdo aprendido. Como possui uma função bem específica sua linguagem se limita a uma linguagem visual de imagens, escolhidas pelos alunos ou professor pode ter um diferente enfoque, que permite uma variedade na linguagem visual.";
+                break;
+            case ItemName.Jornais:
+                FriendlyName = "Jornais";
+                Description = "A inflação subiu de novo… Desse jeito onde será que as coisas vão parar?";
+                FullDescription = "Esta mídia teve seu início no formato impresso. É utilizada para transmitir informações, contém um gênero textual amplamente estudado em sala de aula, expressam diferentes perspectivas políticas, econômicas, culinárias, de lazer, entre outras que são produzidas socialmente, registram os acontecimentos históricos da época.  É acessível somente a pessoas letradas. Sua prática pedagógica serve para introduzir os estudantes nos conhecimentos da alfabetização, demonstra a diferença de produção textual na perspectiva da diversidade de opiniões, modos, vertentes no ensino e aprendizagem de conteúdos. Assim como os livros didáticos, traz texto escrito complementado por imagens, porém com um estilo de escrita geralmente diferente.";
+                break;
+            case ItemName.Fotografia:
+                FriendlyName = "Fotografia";
+                Description = "É a foto de um pássaro local pousado em um banco de escola";
+                FullDescription = "É um recurso imagético de expressão de amplo uso. Seu uso pedagógico serve para ilustrar, apresentar e demonstrar conteúdo, permitir a elaboração de novos aprendizados e ampliação de conhecimento. O estudante usa para expressar ideias, ilustrar a elaboração dos conhecimentos aprendidos, apresentar e socializar com o grande grupo. Sua prática social é a de guardar recordações, registrar momentos e locais, denunciar fatos e divulgação. Esta mídia de linguagem não verbal, serve perfeitamente para trabalhar com imagens específicas.";
+                break;
+            case ItemName.QuadroNegro:
+                FriendlyName = "Quadro Negro";
+                Description = "Hmmm, é bom lembrar que tanto o professor quanto os alunos podem usar essa mídia. O velho companheiro dos professores.";
+                FullDescription = "É um espaço plano reutilizável geralmente riscado com giz branco para transmissão de conteúdo.  Exposição da matéria para grande quantidade de estudantes. Possibilita ao aluno expor suas elaborações sobre os conteúdos e experiências. Tanto o professor quanto o aluno podem utilizá-lo de forma visual, expondo o assunto trabalhado de forma expositiva e/ou colaborativa.";
                 break;
         }
     }
