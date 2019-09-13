a96b383ddd1cd3a271470fb4b996ab97d25a3593
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-09-13 15:48:42 -0300

    Consertar os reverts para o commit do Alexis

--- Assets/Scripts/SistemaDialogo/NpcDialogo.cs
+++ Assets/Scripts/SistemaDialogo/NpcDialogo.cs
@@ -1,57 +1,29 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using GameComenius.Dialogo;
 
 public class NpcDialogo : MonoBehaviour
 {
-    [SerializeField] private Vector2Int questControlIndex;
-
-    [SerializeField] private bool dialogoObrigatorio = false;
-
-    [SerializeField] private float esperaDialogoObrigatorio = 2f;
-
     [SerializeField] private Dialogo dialogoPrincipal = new Dialogo();
 
     [SerializeField] private Dialogo[] dialogosSecundarios = new Dialogo[1];
 
-
-    private void Start()
-    {
-        if (dialogoObrigatorio && !QuestManager.GetQuestControl(questControlIndex)) 
-        {
-            GameManager.uiSendoUsada = true;
-            StartCoroutine(DialogoObrigatorio());
-        }
-    }
+    [HideInInspector] public bool jaFalou = false;
 
     public void OnMouseUp()
     {
-        if (!GameManager.uiSendoUsada)
+        if (!jaFalou)
         {
-            if (!QuestManager.GetQuestControl(questControlIndex))
-            {
-                SistemaDialogo.sistemaDialogo.ComecarDialogo(dialogoPrincipal, this);
-            }
-            else if (dialogosSecundarios.Length > 0) 
-            {
-                int i = Random.Range(0, dialogosSecundarios.Length);
-
-                SistemaDialogo.sistemaDialogo.dialogo = dialogosSecundarios[i];
-                SistemaDialogo.sistemaDialogo.ComecarDialogo(dialogosSecundarios[i], this);
-            }
+            SistemaDialogo.sistemaDialogo.ComecarDialogo(dialogoPrincipal, this);
         }
-    }
+        else
+        {
+            int i = Random.Range(0, dialogosSecundarios.Length);
 
-    private IEnumerator DialogoObrigatorio()
-    {
-        yield return new WaitForSeconds(esperaDialogoObrigatorio);
+            if (i == dialogosSecundarios.Length) { i -= 1; }
 
-        SistemaDialogo.sistemaDialogo.ComecarDialogo(dialogoPrincipal, this);
-    }
-
-    public void SetQuestControl()
-    {
-        QuestManager.SetQuestControl(questControlIndex, true);
+            SistemaDialogo.sistemaDialogo.dialogo = dialogosSecundarios[i];
+            SistemaDialogo.sistemaDialogo.ComecarDialogo(dialogosSecundarios[i], this);
+        }
     }
 }
