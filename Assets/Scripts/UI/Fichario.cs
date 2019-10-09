d9a527a35e01f745fc231b4ef3e3eef16ba49779
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-10-09 05:38:58 -0300

    Trocar entre abas do fichário

---
+++ Assets/Scripts/UI/Fichario.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fichario : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject folhaInventario;
+    [SerializeField]
+    private GameObject folhaMapa;
+    [SerializeField]
+    private GameObject folhaDiario;
+
+    private static GameObject folhaSelecionada;
+
+    // Use this for initialization
+    void Start () {
+        gameObject.SetActive(false);
+
+        if (!folhaSelecionada) folhaSelecionada = folhaInventario;
+        SelecionarInventario();
+
+	}
+
+    public void SelecionarDiario()
+    {
+        if (folhaSelecionada == folhaDiario) return;
+
+        folhaSelecionada = folhaDiario;
+        folhaInventario.transform.SetAsFirstSibling();
+        folhaMapa.transform.SetAsFirstSibling();
+    }
+
+    public void SelecionarMapa()
+	{
+        if (folhaSelecionada == folhaMapa) return;
+
+        folhaSelecionada = folhaMapa;
+        folhaInventario.transform.SetAsFirstSibling();
+        folhaDiario.transform.SetAsFirstSibling();
+    }
+
+    public void SelecionarInventario()
+	{
+        if (folhaSelecionada == folhaInventario) return;
+
+        folhaSelecionada = folhaInventario;
+        folhaMapa.transform.SetAsFirstSibling();
+        folhaDiario.transform.SetAsFirstSibling();
+	}
+}
