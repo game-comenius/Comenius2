bc6af410af1e9495887d8a6f7aa6e13812f83e85
gamecomenius-github <game.comenius@outlook.com>
2019-10-18 16:56:09 -0300

    função de reiniciar o planejamento, ui obscurecendo o mundo

---
+++ Assets/Scripts/UI/FadeEffect.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeEffect : MonoBehaviour {
+
+    bool faded = false;
+    Color tmp;
+
+    // Use this for initialization
+    void Start () {
+        tmp = this.GetComponent<SpriteRenderer>().color;
+        tmp.a = 0f;
+        this.GetComponent<SpriteRenderer>().color = tmp;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (faded) {
+            while (tmp.a < 0.7f) {
+                tmp.a += 0.001f;
+                this.GetComponent<SpriteRenderer>().color = tmp;
+                //Debug.Log("alpha = " + tmp.a);
+            }
+            tmp.a = 0.7f;
+        }
+        else {
+            while (tmp.a > 0f)
+            {
+                tmp.a -= 0.001f;
+                this.GetComponent<SpriteRenderer>().color = tmp;
+                //Debug.Log("alpha = " + tmp.a);
+            }
+            tmp.a = 0f;
+        }
+	}
+
+    public void Fadeout() {
+        //this.GetComponent<SpriteRenderer>().enabled = true;
+        faded = true;
+    }
+
+    public void Fadein() {
+        //this.GetComponent<SpriteRenderer>().enabled = false;
+        faded = false;
+    }
+}
