3727fc6ecadf4ea03950c183472a35827b4fe0af
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-11-10 17:13:03 -0300

    Excluir automaticamente as mídias padrão no custom

---
+++ Assets/Scripts/CustomGame/CustomPlanejamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPlanejamento : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AnularQuestsParaIniciarPlanejamento();
+
+        ZerarMidiasPadraoDoProfessor();
+	}
+
+    private void AnularQuestsParaIniciarPlanejamento()
+    {
+        var objetoQueIniciaPlanejamento = FindObjectOfType<Plan>();
+        var quest = objetoQueIniciaPlanejamento.GetComponent<QuestScript>();
+        quest.enabled = false;
+        var c = objetoQueIniciaPlanejamento.GetComponent<PolygonCollider2D>();
+        c.enabled = true;
+    }
+
+    private void ZerarMidiasPadraoDoProfessor()
+    {
+        var planejamento = GameObject.Find("PlanejamentoGameObject");
+
+        var momentos = planejamento.GetComponentsInChildren<MidiaMomento>(true);
+        foreach (var momento in momentos)
+        {
+            momento.initialItem = ItemName.SemNome;
+            momento.AddItem(ItemName.SemNome);
+        }
+    }
+}
