c588b62b349e3b6448f4b29365939f20dfc4c6f8
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-11-10 02:42:37 -0300

    Atualizar a interface do planejamento

---
+++ Assets/Scripts/UI/OnHoverToggler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnHoverToggler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    [SerializeField]
+    private GameObject objectThatWillBeToggled;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        objectThatWillBeToggled.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        objectThatWillBeToggled.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+        objectThatWillBeToggled.SetActive(false);
+	}
+
+
+}
