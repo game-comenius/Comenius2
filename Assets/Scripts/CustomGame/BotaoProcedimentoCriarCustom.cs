e02bffe3246a23a650a8b1cf53dfeb082d8fbb80
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-11-13 09:39:35 -0300

    Adicionar escolhas de procedimentos no custom (início)

---
+++ Assets/Scripts/CustomGame/BotaoProcedimentoCriarCustom.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BotaoProcedimentoCriarCustom : MonoBehaviour, IPointerClickHandler
+{
+    private Procedimento procedimento;
+    public Procedimento Procedimento
+    {
+        get
+        {
+            return procedimento;
+        }
+
+        set
+        {
+            procedimento = value;
+            UpdateSprite();
+        }
+    }
+
+    private MomentoUICriarCustom meuMomento;
+
+    // UI fields
+    private Image image;
+    private static Color selectedColor = new Color(1, 1, 1, 1);
+    private static Color unselectedColor = new Color(.5f, .5f, .5f, .6f);
+
+    void Awake () {
+        image = GetComponent<Image>();
+        // Começa não selecionado
+        UpdateColor(false);
+	}
+
+    private void UpdateSprite()
+    {
+        image.sprite = ProcedimentoSpriteDatabase.SpriteOf(procedimento);
+        image.preserveAspect = true;
+    }
+
+    public void UpdateColor(bool selected)
+    {
+        image.color = selected ? selectedColor : unselectedColor;
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        meuMomento.SelecionarProcedimento(this);
+    }
+
+    public void CadastrarMeuMomento(MomentoUICriarCustom meuMomento)
+    {
+        this.meuMomento = meuMomento;
+    }
+}
---
+++ Assets/Scripts/CustomGame/ProcedimentoSpriteDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcedimentoSpriteDatabase : MonoBehaviour {
+
+    [System.Serializable]
+    private struct ProcedimentoSprite
+    {
+        public Procedimento procedimento;
+        public Sprite sprite;
+    }
+
+    [SerializeField]
+    private List<ProcedimentoSprite> procedimentosSprites;
+
+    private static ProcedimentoSpriteDatabase Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            transform.parent = null;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static Sprite SpriteOf(Procedimento procedimento)
+    {
+        var list = Instance.procedimentosSprites;
+        var element = list.Find(x => x.procedimento == procedimento);
+        return element.sprite;
+    }
+}
