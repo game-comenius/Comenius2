293d8a1013a7c8b972e36a68e7ea727255ae5b15
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-11-17 19:08:10 -0300

    Alterar alpha da porta (professores) ao passar o mouse

--- Assets/Scripts/UI/DynamicCursorForDoors.cs
+++ Assets/Scripts/UI/DynamicCursorForDoors.cs
@@ -10,14 +10,20 @@ public class DynamicCursorForDoors : MonoBehaviour, IPointerEnterHandler, IPoint
     private CursorMode curmode;
     private Vector2 hotspot;
 
+    private static Color selectedColor = new Color(1, 1, 1, 1);
+    private static Color unselectedColor = new Color(1, 1, 1, 0.4f);
+    private SpriteRenderer spriteRenderer;
+
     public void OnMouseEnter()
     {        
         Cursor.SetCursor(select, hotspot, curmode);
+        if (spriteRenderer) spriteRenderer.color = selectedColor;
     }
 
     public void OnMouseExit()
     {
         Cursor.SetCursor(cursorImage, hotspot, curmode);
+        if (spriteRenderer) spriteRenderer.color = unselectedColor;
     }
 
     // Use this for initialization
@@ -27,17 +33,14 @@ public class DynamicCursorForDoors : MonoBehaviour, IPointerEnterHandler, IPoint
         select = GameObject.FindObjectOfType<Cursor1>().select;
         curmode = GameObject.FindObjectOfType<Cursor1>().curmode;
         hotspot = GameObject.FindObjectOfType<Cursor1>().hotspot;
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer) spriteRenderer.color = unselectedColor;
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-            Cursor.SetCursor(select, hotspot, curmode);
+        Cursor.SetCursor(select, hotspot, curmode);
     }
 
     public void OnPointerExit(PointerEventData eventData)
