8e1ad021894e7dd9bba86134d1b0e6962a48014c
game-comenius <game.comenius@outlook.com>
2019-11-19 15:55:07 -0300

    tutoriais
    Co-Authored-By: Alexis Mendes Sequeira <alexisms@users.noreply.github.com>

---
+++ Assets/Scripts/UI/FadePatio1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadePatio1 : MonoBehaviour
+{
+
+    Color tmp;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        if (gameObject.tag == "fadeMenu")
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        tmp = this.GetComponent<SpriteRenderer>().color;
+        tmp.a = 1f;
+        this.GetComponent<SpriteRenderer>().color = tmp;
+
+    }
+
+    IEnumerator FadeBackground(bool faded)
+    {
+        if (faded)
+        {
+            for (float i = 0.7f; i >= 0; i -= Time.deltaTime)
+            {
+                tmp.a = i;
+                this.GetComponent<SpriteRenderer>().color = tmp;
+                yield return null;
+            }
+        }
+        else
+        {
+            for (float i = 0; i <= 0.7f; i += Time.deltaTime)
+            {
+                tmp.a = i;
+                this.GetComponent<SpriteRenderer>().color = tmp;
+                yield return null;
+            }
+        }
+    }
+
+    public void Fadeout()
+    {
+        StartCoroutine(FadeBackground(false));
+    }
+
+    public void Fadein()
+    {
+        StartCoroutine(FadeBackground(true));
+    }
+}
