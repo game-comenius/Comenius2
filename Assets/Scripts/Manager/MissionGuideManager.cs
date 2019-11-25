d5e3b2458903b9167ac9c4f7ac547d31b96cb509
AlexisMS <alexisora@gmail.com>
2019-11-25 01:28:22 -0300

    Trabalhando na parte de missão do fichário

---
+++ Assets/Scripts/Manager/MissionGuideManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MissionGuideManager : MonoBehaviour
+{
+
+    private static MissionGuideManager _missionGuideManager;
+
+    public static MissionGuideManager missionGuideManager
+    {
+        get
+        {
+            return _missionGuideManager;
+        }
+    }
+
+    public string[] missions = new string[3];
+
+    private TextMeshProUGUI missionTip;
+
+    private void Awake()
+    {
+        if (_missionGuideManager == null)
+        {
+            _missionGuideManager = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        missionTip = GameObject.Find("TextoMissao").GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SetMissionGuide(int index)
+    {
+        missionTip.SetText(missions[index]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
