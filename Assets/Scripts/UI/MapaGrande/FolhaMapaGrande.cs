d599f8207e358b8c12a67dc418099f7fd397c00f
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2019-11-26 00:57:11 -0300

    Iniciar implementação do mapa no fichário

---
+++ Assets/Scripts/UI/MapaGrande/FolhaMapaGrande.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolhaMapaGrande : MonoBehaviour {
+
+    public LocalNoMapa PatioEsquerdo;
+    public LocalNoMapa PatioDireito;
+    public LocalNoMapa SalaDiretor;
+    public LocalNoMapa Coordenacao;
+    public LocalNoMapa SalaProfessores;
+    public LocalNoMapa Biblioteca;
+    public LocalNoMapa SalaMultimeios;
+    public LocalNoMapa SalaInformatica;
+    public LocalNoMapa SalaAulaCelestino;
+    public LocalNoMapa SalaAulaPaulino;
+    public LocalNoMapa SalaAulaVladmir;
+    public LocalNoMapa SalaAulaJean;
+
+
+    // Use this for initialization
+    void Start () {
+        //PatioEsquerdo.NomeDaCena = "Patio_1";
+        //PatioDireito.NomeDaCena = null;
+        //SalaDiretor.NomeDaCena = null;
+        //Coordenacao.NomeDaCena = "CoordenacaoPedagogica";
+        //SalaProfessores.NomeDaCena = "SalaProfessores";
+        //Biblioteca.NomeDaCena = "Biblioteca";
+        SalaMultimeios.NomeDaCena = "Multimeios";
+        //SalaInformatica.NomeDaCena = null;
+        //SalaAulaCelestino.NomeDaCena = null;
+        //SalaAulaPaulino.NomeDaCena = null;
+        //SalaAulaVladmir.NomeDaCena = null;
+        //SalaAulaJean.NomeDaCena = "Sala_de_aula_Jean";
+
+        gameObject.SetActive(false);
+    }
+
+    public bool Teletransportar(LocalNoMapa local)
+    {
+        var cena = local.NomeDaCena;
+        if (cena == null)
+        {
+            return false;
+        }
+        else
+        {
+            GetComponent<SceneLoader>().LoadNewScene(cena);
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MapaGrande/LocalNoMapa.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LocalNoMapa : MonoBehaviour, IPointerClickHandler {
+
+    public string NomeDaCena { get; set; }
+
+    private FolhaMapaGrande mapa;
+
+    // Use this for initialization
+    void Start () {
+        mapa = GetComponentInParent<FolhaMapaGrande>();
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        mapa.Teletransportar(this);
+    }
+}
