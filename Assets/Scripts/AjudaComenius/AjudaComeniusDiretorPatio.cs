768de4b935293eb5c0cc449ef917302e8708d20d
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-01-05 19:34:32 -0300

    Aplicar FadeEffect a ajuda do Comenius

--- Assets/Scripts/AjudaComenius/AjudaComeniusDiretorPatio.cs
+++ Assets/Scripts/AjudaComenius/AjudaComeniusDiretorPatio.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AjudaComeniusDiretorPatio : MonoBehaviour {
 
@@ -10,11 +11,14 @@ public class AjudaComeniusDiretorPatio : MonoBehaviour {
     private bool permiteFechar;
 
     private Canvas canvas;
+    private FadeEffect backgroundFadeEffect;
 
     // Use this for initialization
     private void Start () {
         canvas = GetComponentInChildren<Canvas>();
 
+        backgroundFadeEffect = GetComponentInChildren<FadeEffect>();
+        
         // Cadastrar função para ser invocada quando o diretor fechar o diálogo
         diretor.OnEndDialogueEvent += Mostrar;
 	}
@@ -23,9 +27,18 @@ public class AjudaComeniusDiretorPatio : MonoBehaviour {
     {
         GameManager.UISendoUsada();
         canvas.enabled = true;
+        backgroundFadeEffect.MaxAlpha = 0.6f;
+        StartCoroutine(backgroundFadeEffect.Fade());
 
         var coroutine = PermitirFecharApos(8);
         StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Fechar()
+    {
+        yield return StartCoroutine(backgroundFadeEffect.Fade());
+        canvas.enabled = false;
+        GameManager.UINaoSendoUsada();
     }
 
     private IEnumerator PermitirFecharApos(float segundos)
@@ -37,9 +50,6 @@ public class AjudaComeniusDiretorPatio : MonoBehaviour {
     private void Update()
     {
         if (permiteFechar && Input.anyKeyDown)
-        {
-            canvas.enabled = false;
-            GameManager.UINaoSendoUsada();
-        }
+            StartCoroutine(Fechar());
     }
 }
