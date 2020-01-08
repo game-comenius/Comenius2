0d2c0b10d80708b60dbaffcd168a9d251861979d
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-01-08 01:18:47 -0300

    Implementar janela de missões (início)

---
+++ Assets/Scripts/AjudaComenius/AjudaComeniusJanelaMissoes.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AjudaComeniusJanelaMissoes : MonoBehaviour {
+
+    // A ajuda do comenius vai aparecer logo depois do diálogo com o Jean
+    // por isso precisamos de uma referência ao Jean neste caso
+    [SerializeField]
+    private NpcDialogo jean;
+
+    [SerializeField]
+    private TextMeshProUGUI texto;
+
+    [SerializeField]
+    private JanelaMissoes janelaMissoes;
+
+    private readonly string[] falas =
+    {
+        "Muito bem Lurdinha! Agora que você falou com o professor Jean e já sabe sua missão, você pode consultá-la sempre que quiser aqui.",
+        "No momento você têm uma missão principal e pequenas tarefas que podem te ajudar na missão em questão. Clique nos tópicos para ver seus objetivos!",
+        "Ótimo! Agora você pode ir até a sala multimeios onde te explicarei mais coisas. Te encontro lá!"
+    };
+
+    private bool permiteFechar;
+
+    private Canvas canvas;
+    private FadeEffect backgroundFadeEffect;
+
+    // Use this for initialization
+    private void Start()
+    {
+        canvas = GetComponentInChildren<Canvas>();
+
+        backgroundFadeEffect = GetComponentInChildren<FadeEffect>();
+
+        // Cadastrar função para ser invocada quando o diretor fechar o diálogo
+        jean.OnEndDialogueEvent += Mostrar;
+    }
+
+    private void Mostrar()
+    {
+        GameManager.UISendoUsada();
+
+        texto.text = falas[0];
+        canvas.enabled = true;
+
+        backgroundFadeEffect.MaxAlpha = 0.6f;
+        StartCoroutine(backgroundFadeEffect.Fade());
+
+        var coroutine = PermitirFecharApos(8);
+        StartCoroutine(coroutine);
+    }
+
+    private IEnumerator Fechar()
+    {
+        yield return StartCoroutine(backgroundFadeEffect.Fade());
+        canvas.enabled = false;
+        GameManager.UINaoSendoUsada();
+    }
+
+    private IEnumerator PermitirFecharApos(float segundos)
+    {
+        yield return new WaitForSeconds(segundos);
+        permiteFechar = true;
+    }
+
+    private void Update()
+    {
+        if (permiteFechar && Input.anyKeyDown)
+            StartCoroutine(Fechar());
+    }
+
+}
---
+++ Assets/Scripts/Quest/JanelaMissoes/JanelaMissoes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JanelaMissoes : MonoBehaviour {
+
+
+    private bool aberta;
+    private RectTransform transformJanela;
+    private Vector2 posicaoFechada;
+    private Vector2 posicaoAberta;
+
+	// Use this for initialization
+	void Start () {
+        transformJanela = transform.GetChild(0).GetChild(0).GetComponent<RectTransform>();
+        posicaoFechada = transformJanela.anchoredPosition;
+        posicaoAberta = new Vector2(32, 0);
+    }
+
+    public void Toggle()
+    {
+        transformJanela.anchoredPosition = (aberta) ? posicaoFechada : posicaoAberta;
+        aberta = !aberta;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
