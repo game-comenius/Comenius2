9745d9498b3039df3b557aa010be6c2088b81f26
AlexisMS <alexisora@gmail.com>
2020-02-18 21:35:35 -0300

    Implementando mídias da missão 2

--- Assets/Scripts/Quest/Missions/Mission2.cs
+++ Assets/Scripts/Quest/Missions/Mission2.cs
@@ -22,6 +22,7 @@ public class Mission2 : MonoBehaviour
         new QuestClass (10200, "Fale com o Vladmir. (2)", new DoQuest(), new int[]{ 10100, -5 }),
         new QuestClass (10300, "Fale com o Vladmir. (3)", new DoQuest(), new int[]{ 10200, -5 }),
         new QuestClass (10400, "Falar com o 111 no Pátio.", new DoQuest(), new int[]{ }),
+        new QuestClass (10401, "Controle: Pegar fotografia com o garoto estranho.", new DoQuest(), new int[]{ 10400 }),
         new QuestClass (10500, "Falar com o 021 no Pátio.", new DoQuest(), new int[]{ }),
         new QuestClass (10600, "Falar com o 031 no Pátio.", new DoQuest(), new int[]{ }),
         new QuestClass (10700, "Falar com o Garoto Ambiente no Pátio 2.", new DoQuest(), new int[]{ }),
@@ -33,7 +34,10 @@ public class Mission2 : MonoBehaviour
         new QuestClass (11300, "Falar com a Antônia na Coordenação.", new DoQuest(), new int[]{ }),
         new QuestClass (11400, "Falar com o Garota da Literatura na Biblioteca.", new DoQuest(), new int[]{ }),
         new QuestClass (11500, "Falar com o 101 na Biblioteca.", new DoQuest(), new int[]{ }),
-        new QuestClass (11600, "Falar com a Alice na Biblioteca.", new DoQuest(), new int[]{ })
+        new QuestClass (11600, "Falar com a Alice na Biblioteca.", new DoQuest(), new int[]{ }),
+
+        new QuestClass (12000, "Pegar o retroprojetor na sala multimeios.", new DoQuest(), new int[]{ }),
+        new QuestClass (12001, "Controle: Pegar o retroprojetor na sala multimeios.", new DoQuest(), new int[]{ 12000 })
 
     };
 }
