3cc97b905977ab87a5c38763fa713f0d37dfc617
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-03-08 19:37:20 -0300

    Mostrar resultado da sala do Jean no Start()
     - Resultado da missão anterior (missão 1), ou seja, sala suja/limpa, é
       mostrado assim que a Lurdinha entra na sala do Jean
     - Um script PublicMethodCaller foi criado para executar a mudança de sala

---
+++ Assets/Scripts/Utility/PublicMethodCaller.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PublicMethodCaller : MonoBehaviour {
+
+    public UnityEvent PublicMethodsToBeCalledOnStart;
+
+    void Start()
+    {
+        PublicMethodsToBeCalledOnStart.Invoke();
+    }
+}
