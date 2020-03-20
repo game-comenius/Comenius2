09146f576a9c95ca23f0903f51234d6973992cdb
IndraRosa <indrakitty@gmail.com>
2020-03-19 23:11:06 -0300

    Efeitos de Áudio
    Adicionados efeitos de som na coleta das mídias das missões 1 e 2.
    
    --- Maicon e Indra

---
+++ Assets/Scripts/AudioControl/gerenciadorEfeitos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gerenciadorEfeitos : MonoBehaviour
+{
+
+    private static gerenciadorEfeitos instance = null;
+    public static gerenciadorEfeitos Instance
+    {
+        get { return instance; }
+    }
+
+    private AudioSource fonteAudio;
+
+    public void PlayMusic()
+    {
+        if (instance.fonteAudio.isPlaying) return;
+        instance.fonteAudio.Play();
+    }
+
+
+}
