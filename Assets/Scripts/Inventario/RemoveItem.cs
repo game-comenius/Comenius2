a2b1b0c737cd1fcfedd13cf0efd3286b94820a44
AlexisMS <alexisora@gmail.com>
2020-04-03 16:28:37 -0300

    Mais revisões da missão 2

---
+++ Assets/Scripts/Inventario/RemoveItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveItem : MonoBehaviour {
+
+    public ItemName target;
+    bool done = false;
+
+    public void deleteItem()
+    {
+        if (!done)
+        {
+            Player.Instance.Inventory.Remove(target);
+            done = true;
+        }
+    }
+}
