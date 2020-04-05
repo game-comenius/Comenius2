7cd14f76df0e49feb7137f863c9274d6217033a6
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-04-05 03:14:06 -0300

    Acrescentar descrição da mídia na coleta
     - Quando a Lurdinha coleta uma mídia, ela fala a descrição geral da
       mídia também. Não testei para todas as coletas, mas deve estar ok.

--- Assets/Scripts/Inventario/CollectItem.cs
+++ Assets/Scripts/Inventario/CollectItem.cs
@@ -1,11 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class CollectItem : MonoBehaviour {
 
     public ItemName target;
-    bool done = false;    
+    bool done = false;
+
+    private void Start()
+    {
+        AcrescentarDescricaoDaMidiaNaFalaDaLurdinha();
+    }
+
+    private void AcrescentarDescricaoDaMidiaNaFalaDaLurdinha()
+    {
+        // Adicionar descrição da mídia no diálogo que a Lurdinha fala ao
+        // conquistar a mídia. Essa é a StandardDescription de Item.cs
+        var npcDialogo = GetComponentInChildren<NpcDialogo>();
+        if (npcDialogo)
+        {
+            // Descricao que será adicionada à fala de Lurdinha
+            var descricao = new Item(target).DescriptionsInMission3.StandardDescription;
+
+            // Extrair o array de falas da Lurdinha
+            var falas = npcDialogo.dialogoPrincipal.nodulos[0].falas;
+            // Redimensionar o array para aceitar mais um paragrafo de fala
+            Array.Resize(ref falas, falas.Length + 1);
+
+            var ultimoIndice = falas.Length - 1;
+            falas[ultimoIndice] = new GameComenius.Dialogo.Fala
+            {
+                fala = descricao,
+                personagem = GameComenius.Dialogo.Personagens.Lurdinha,
+                emocao = GameComenius.Dialogo.Expressao.Serio
+            };
+
+            // Encaixar o array com as (falas originais + fala nova) no
+            // conjunto de falas da Lurdinha
+            npcDialogo.dialogoPrincipal.nodulos[0].falas = falas;
+        }
+    }
 
     public void addItem() {
         if (!done) {
