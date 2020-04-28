a4275d668427882ad5969bfabc6346866635836b
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-04-28 17:31:18 -0300

    Trancar a porta do custom até o planejamento

--- Assets/Scripts/CustomGame/CustomPortaSalaProfessores.cs
+++ Assets/Scripts/CustomGame/CustomPortaSalaProfessores.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CustomPortaSalaProfessores : MonoBehaviour {
 
+    private Planejamento planejamento;
+
 	private IEnumerator Start () {
         // Truque para que a Lurdinha comece no lugar certo na sala de aula
         // porque este script vai enganar o jogo e a Lurdinha vai ignorar
@@ -11,10 +13,19 @@ public class CustomPortaSalaProfessores : MonoBehaviour {
         yield return new WaitUntil(() => Player.Instance != null);
         Player.Instance.sceneName = "M1_Patio1_Dia";
 
-        // Trocar o destino de todas as portas na sala dos professores para
-        // a sala de aula selecionada pelo criador do jogo custom
         var doors = FindObjectsOfType<DoorTransition>();
         foreach (var door in doors)
+        {
+            // Trocar o destino de todas as portas na sala dos professores para
+            // a sala de aula selecionada pelo criador do jogo custom
             door.sceneName = "CustomSalaDeAula";
+
+            // Desabilitar a porta até que o jogador confirme o planejamento
+            door.GetComponent<PolygonCollider2D>().enabled = false;
+            FindObjectOfType<Planejamento>().QuandoConfirmarPlanejamentoEvent += () =>
+            {
+                door.GetComponent<PolygonCollider2D>().enabled = true;
+            };
+        }
 	}
 }
