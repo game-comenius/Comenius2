6d5859a123c8bf5bacb90e8650feac914fa2620a
Bruno <40030516+Bruno-12-1-UFSC@users.noreply.github.com>
2020-04-27 23:35:10 -0300

    Mostrar botão do fichário na missão 2 e 3

--- Assets/Scripts/UI/Fichario/BotaoAbrirFichario.cs
+++ Assets/Scripts/UI/Fichario/BotaoAbrirFichario.cs
@@ -63,6 +63,16 @@ public class BotaoAbrirFichario : MonoBehaviour, IPointerClickHandler {
         image.enabled = visivel;
         Ativo = true;
         #endif
+        // Essa linha está aqui para facilitar o desenvolvimento do jogo.
+        // Ativar este botão se a equipe de desenvolvimento executar o jogo
+        // direto da missão 2 ou direto da missão 3.
+        var currentScene = SceneManager.GetActiveScene().name;
+        if (currentScene.Equals("M2_Patio1_inicio") || currentScene.Equals("M3_Patio1_Chamado"))
+        {
+            visivel = true;
+            image.enabled = visivel;
+            Ativo = true;
+        }
     }
 
     void Start() {
